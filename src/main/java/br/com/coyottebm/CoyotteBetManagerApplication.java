43854425ae7214a1065e798584d4efa1b2af40ac
DESKTOP-4GBVCLU\vinic <flambres3@gmail.com>
2020-12-06 18:07:13 -0300

    Commit inicial do projeto.

---
+++ src/main/java/br/com/coyottebm/CoyotteBetManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.coyottebm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoyotteBetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoyotteBetManagerApplication.class, args);
+	}
+
+}
