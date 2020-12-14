e451dbe56d5cb4a7233cddef72158fc19ff5b7b2
DESKTOP-4GBVCLU\vinic <flambres3@gmail.com>
2020-12-13 21:22:22 -0300

    Remoção do Auto Increment da classe Usuário.

--- src/main/java/br/com/coyottebm/model/Usuario.java
+++ src/main/java/br/com/coyottebm/model/Usuario.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -20,7 +18,6 @@ import lombok.Setter;
 public class Usuario {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NonNull
